3e8d620c7950f3b834648610c3fe6da682688d2e
Farhan <bizimpulse@gmail.com>
2023-12-05 19:02:49 +0800

    Update

--- src/applications/app.state.ts
+++ src/applications/app.state.ts
@@ -1,10 +1,15 @@
 import { writable, type Writable } from "svelte/store";
+import { UserState } from "./user.state";
 
 class _App {
+	userState: UserState;
+	constructor(userState: UserState) {
+		this.userState = userState;
+	}
 	width: Writable<number> = writable(0);
 	height: Writable<number> = writable(0);
 	loading: Writable<boolean> = writable(false);
 	error: Writable<any> = writable(undefined);
 }
 
-export const AppState = new _App();
+export const AppState = new _App(UserState);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,23 @@
 import type { LoginInput } from "$api/routes/auth/auth.schema";
+import { browser } from "$app/environment";
 import { UserState } from "$applications";
 import { PUBLIC_API_BASE_PATH } from "$env/static/public";
-import type { ReceivedToken } from "$types";
+import type { DecodedToken, ReceivedToken } from "$types";
+import { jwtDecode } from "jwt-decode";
 import { HttpService } from "./http.service";
 
 class _AuthService {
 	path = PUBLIC_API_BASE_PATH + "/auth";
+
+	constructor(private userState: UserState) {
+		this.userState.accessToken.subscribe((token) => {
+			if (browser) {
+				if (token) {
+					this.fetchUserFromLocalAccessToken(token);
+				}
+			}
+		});
+	}
 
 	async login(input: LoginInput) {
 		const url = this.path + "/login";
@@ -22,9 +34,7 @@ class _AuthService {
 			});
 		}
 
-		UserState.accessToken.set(token.accessToken);
-		UserState.refreshToken.set(token.refreshToken);
-		UserState.permissions.set(token.permissions);
+		this.saveToken(token);
 	}
 
 	async refresh() {
@@ -35,10 +45,33 @@ class _AuthService {
 			credentials: "include"
 		});
 
-		UserState.accessToken.set(token.accessToken);
-		UserState.refreshToken.set(token.refreshToken);
-		UserState.permissions.set(token.permissions);
+		this.saveToken(token);
+	}
+
+	private saveToken(receivedToken: ReceivedToken) {
+		this.userState.accessToken.set(receivedToken.accessToken);
+		this.userState.refreshToken.set(receivedToken.refreshToken);
+		this.userState.permissions.set(receivedToken.permissions);
+	}
+
+	private validateAccessToken(accessToken: string) {
+		const decodedToken: DecodedToken = jwtDecode(accessToken);
+		const validExpiry = this.validateTokenExpiry(decodedToken);
+
+		if (validExpiry) {
+			//
+		} else {
+			//
+		}
+	}
+
+	private validateTokenExpiry(token: DecodedToken): boolean {
+		return new Date() < new Date(token.exp * 1000);
+	}
+
+	private fetchUserFromLocalAccessToken(token: string) {
+		this.validateAccessToken(token);
 	}
 }
 
-export const AuthService = new _AuthService();
+export const AuthService = new _AuthService(UserState);
